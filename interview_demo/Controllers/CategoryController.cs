dca7e29d0cf71b8a452335204e4cacf4414b6b25
Laban <laban@wegames.com.tw>
2019-07-16 17:06:32 +0800

    Implement index page

---
+++ interview_demo/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using interview_demo.DAL;
+using interview_demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace interview_demo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryContext db = new CategoryContext();
+        
+        //首頁
+        public ActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
