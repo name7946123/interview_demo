fcfa918381ca4246d9f26c330d8d8c7cee6e286b
Laban <laban@wegames.com.tw>
2019-07-16 16:46:10 +0800

    Create DB and model

---
+++ interview_demo/DAL/CategoryContext.cs
@@ -0,0 +1,14 @@
+﻿using interview_demo.Models;
+using System.Data.Entity;
+
+namespace interview_demo.DAL
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext() : base("CategoryContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ interview_demo/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace interview_demo.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime modified_at { get; set; }
+        public bool is_published { get; set; }
+    }
+}
